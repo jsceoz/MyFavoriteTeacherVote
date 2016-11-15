4f9c8d82ac67ef03f054a721e8b50abd72683ad2
Jsceoz <jsceoz96@gmail.com>
2016-11-16 07:35:01 +0800

    修改时区

--- teachervote/settings.py
+++ teachervote/settings.py
@@ -138,6 +138,6 @@ REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework.authentication.TokenAuthentication',
     # ),
-    'PAGE_SIZE': 8,
+    # 'PAGE_SIZE': 8,
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
